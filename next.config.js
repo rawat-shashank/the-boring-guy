325ffe515634e0d8499005fab992f1b7de1b06ab
rawat-shashank <shashank17nov@gmail.com>
2021-06-06 17:00:26 +0530

    ISR, lighthouse performance fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   assetPrefix: "/the-boring-guy/",
   basePath: "/the-boring-guy",
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
